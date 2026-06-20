02172dead45bc2b64e5e540dec4e223d9432a978
Simon Clur <simon.clur@rooivalk.com.au>
2026-06-21 08:19:56 +1000

    Remove temp scripts

--- fix_initial_ui.py
+++
@@ -1,7 +0,0 @@
-with open('index.html', 'r') as f:
-    text = f.read()
-
-text = text.replace("const loadedMatchDetails = {};", "const loadedMatchDetails = {};\n        let isInitialUI = true;")
-
-with open('index.html', 'w') as f:
-    f.write(text)
